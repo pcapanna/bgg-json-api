3ccad960251711c8ba1ba7f06605e9957b02553c
Patricio Capanna <patriciocapanna@gmail.com>
2020-07-25 07:43:47 -0300

    Correcting npm start

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -12,7 +11,6 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
